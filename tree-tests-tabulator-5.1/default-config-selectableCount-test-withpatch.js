212c0f85d8a92f7b1cec2671a48211bc7d538bb8
Bruno Harbulot <bruno@distributedmatter.net>
2022-02-10 12:57:56 +0000

    Added selectableCount patch example.

---
+++ tree-tests-tabulator-5.1/default-config-selectableCount-test-withpatch.js
@@ -0,0 +1,3 @@
+window.__LOCAL_CONFIG.JS_HREF_MAP.tabulator =
+    "https://raw.githubusercontent.com/harbulot/tabulator/selectable_count_dist/dist/js/tabulator.js";
+window.__LOCAL_CONFIG.JS_BYPASS_CORB_MAP.tabulator = true;
